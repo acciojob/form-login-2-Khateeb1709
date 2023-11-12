f056646f786ac3acd21aab8bc53b6793e9e74c08
Khateeb_Rehman <112754572+Khateeb1709@users.noreply.github.com>
2023-11-12 16:10:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function myFunction(){
+	var fn = document.getElementById("fname").value;
+	var ln = document.getElementById("lname").value;
+	var ph = document.getElementById("contact").value;
+	var em = document.getElementById("emailId").value;
+
+	alert("First Name: " + fn + "\n" + "Last Name: " + ln + "\n" + "Phone Number: " + ph + "\n" + "Email ID: " + em);
+	
+}
